722a5c32dcc07783fa3e19442114a263c8e9a6c7
Zeno Popovici <zeno@graffino.com>
2022-11-23 03:46:30 +0200

    feat: load vuetify

--- src/main.js
+++ src/main.js
@@ -4,8 +4,23 @@ import App from './App.vue'
 // Custom CSS
 import './assets/main.css'
 
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+
 // Plugins
 import { registerPlugins } from "@/plugins";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
 // Register plugins
 const app = createApp(App);
 registerPlugins(app);
+
+createApp(App).use(vuetify).mount("#app");
